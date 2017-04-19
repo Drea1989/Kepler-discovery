d31dbd8eebc3227bf9da641b9c93784312b56259
Andrea Balzano <andrea.balzano@live.it>
2017-04-19 15:14:39 +1000

    chore: move functions to helper
    moved functions to the helper file

--- helper.py
+++ helper.py
@@ -115,4 +115,149 @@ def vs_class_imbalance(X,y,X_resampled,y_resampled):
                   ncol=2, labelspacing=0.)
     plt.tight_layout(pad=3)
     plt.show()
-    
+
+
+# For each feature find the data points with extreme high or low values
+def find_outliers(log_data):
+    indices = []
+    for feature in log_data.keys():
+        # TODO: Calculate Q1 (25th percentile of the data) for the given feature
+        Q1 = np.percentile(log_data[feature], 25)
+
+        # TODO: Calculate Q3 (75th percentile of the data) for the given feature
+        Q3 = np.percentile(log_data[feature], 75)
+
+        # TODO: Use the interquartile range to calculate an outlier step (1.5 times the interquartile range)
+        step = (Q3 - Q1) * 1.5
+
+        # Display the outliers
+        # print "Data points considered outliers for the feature '{}':".format(feature)
+        # display(data[~((data[feature] >= Q1 - step) & (data[feature] <= Q3 + step))])
+        indices.extend(log_data[~((log_data[feature] >= Q1 - step) & (log_data[feature] <= Q3 + step))].index.tolist())
+
+    # look for indices that repeats at least twice
+    from collections import Counter
+
+    cnt = Counter()
+    for index in indices:
+        cnt[index] += 1
+    return cnt
+
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test):
+    '''
+    inputs:
+       - learner: the learning algorithm to be trained and predicted on
+       - sample_size: the size of samples (number) to be drawn from training set
+       - X_train: features training set
+       - y_train: income training set
+       - X_test: features testing set
+       - y_test: income testing set
+    '''
+    from sklearn.metrics import fbeta_score, accuracy_score
+    results = {}
+
+    # Fit the learner to the training data using slicing with 'sample_size'
+    start = time()  # Get start time
+    learner = learner.fit(X_train[:sample_size], y_train[:sample_size])
+    end = time()  # Get end time
+    results['train_time'] = end - start
+
+    # Get the predictions on the test set,
+    # then get predictions on the first 300 training samples
+    start = time()  # Get start time
+    predictions_test = learner.predict(X_test)
+    predictions_train = learner.predict(X_train[:300])
+    end = time()  # Get end time
+
+    # Calculate the total prediction time
+    results['pred_time'] = end - start
+
+    # Compute accuracy on the first 300 training samples
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+
+    # Compute accuracy on test set
+    results['acc_test'] = accuracy_score(y_test, predictions_test)
+
+    # Compute F-score on the the first 300 training samples
+    results['f_train'] = fbeta_score(y_train[:300], predictions_train, beta=1.5)
+
+    # Compute F-score on the test set
+    results['f_test'] = fbeta_score(y_test, predictions_test, beta=1.5)
+
+    # Success
+    print "{} trained on {} samples.".format(learner.__class__.__name__, sample_size)
+
+    # Return the results
+    return results
+
+
+def evaluate(results, accuracy, f1):
+    """
+    Visualization code to display results of various learners.
+
+    inputs:
+      - learners: a list of supervised learners
+      - stats: a list of dictionaries of the statistic results from 'train_predict()'
+      - accuracy: The score for the naive predictor
+      - f1: The score for the naive predictor
+    """
+
+    # Create figure
+    fig, ax = plt.subplots(2, 3, figsize=(11, 7))
+
+    # Constants
+    bar_width = 0.3
+    colors = ['#A00000', '#00A0A0', '#00A000']
+
+    # Super loop to plot four panels of data
+    for k, learner in enumerate(results.keys()):
+        for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
+            for i in np.arange(3):
+                # Creative plot code
+                ax[j / 3, j % 3].bar(i + k * bar_width, results[learner][i][metric], width=bar_width, color=colors[k])
+                ax[j / 3, j % 3].set_xticks([0.45, 1.45, 2.45])
+                ax[j / 3, j % 3].set_xticklabels(["1%", "10%", "100%"])
+                ax[j / 3, j % 3].set_xlabel("Training Set Size")
+                ax[j / 3, j % 3].set_xlim((-0.1, 3.0))
+
+    # Add unique y-labels
+    ax[0, 0].set_ylabel("Time (in seconds)")
+    ax[0, 1].set_ylabel("Accuracy Score")
+    ax[0, 2].set_ylabel("F-score")
+    ax[1, 0].set_ylabel("Time (in seconds)")
+    ax[1, 1].set_ylabel("Accuracy Score")
+    ax[1, 2].set_ylabel("F-score")
+
+    # Add titles
+    ax[0, 0].set_title("Model Training")
+    ax[0, 1].set_title("Accuracy Score on Training Subset")
+    ax[0, 2].set_title("F-score on Training Subset")
+    ax[1, 0].set_title("Model Predicting")
+    ax[1, 1].set_title("Accuracy Score on Testing Set")
+    ax[1, 2].set_title("F-score on Testing Set")
+
+    # Add horizontal lines for naive predictors
+    ax[0, 1].axhline(y=accuracy, xmin=-0.1, xmax=3.0, linewidth=1, color='k', linestyle='dashed')
+    ax[1, 1].axhline(y=accuracy, xmin=-0.1, xmax=3.0, linewidth=1, color='k', linestyle='dashed')
+    ax[0, 2].axhline(y=f1, xmin=-0.1, xmax=3.0, linewidth=1, color='k', linestyle='dashed')
+    ax[1, 2].axhline(y=f1, xmin=-0.1, xmax=3.0, linewidth=1, color='k', linestyle='dashed')
+
+    # Set y-limits for score panels
+    ax[0, 1].set_ylim((0, 1))
+    ax[0, 2].set_ylim((0, 1))
+    ax[1, 1].set_ylim((0, 1))
+    ax[1, 2].set_ylim((0, 1))
+
+    # Create patches for the legend
+    patches = []
+    for i, learner in enumerate(results.keys()):
+        patches.append(mpatches.Patch(color=colors[i], label=learner))
+    plt.legend(handles=patches, bbox_to_anchor=(-.80, 2.53), \
+               loc='upper center', borderaxespad=0., ncol=3, fontsize='x-large')
+
+    # Aesthetics
+    plt.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize=16, y=1.10)
+    plt.tight_layout()
+    plt.show()
+
