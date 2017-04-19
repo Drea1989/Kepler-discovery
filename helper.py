c0dfda6fca71e5b2b9f52e7d409edae9f304ed58
Andrea Balzano <andrea.balzano@live.it>
2017-04-19 20:54:30 +1000

    Chore: code cleanup
    cleanup, removed non used code and added comments

--- helper.py
+++ helper.py
@@ -3,6 +3,8 @@
 Created on Thu Apr 13 11:07:07 2017
 
 @author: andre
+some functions are taken from previous Udacity projects  like boston housing and customer segmentation.
+visualisation of class imbalance is an adaptation of the example provided in the documentation
 """
 import numpy as np
 import pandas as pd
@@ -15,64 +17,6 @@ import matplotlib.patches as mpatches
 from sklearn.metrics import fbeta_score, accuracy_score
 from sklearn.decomposition import PCA
 
-#plot stats
-def stats_plots_label(df,labels):
-    means1 = df[labels==0].mean(axis=1)
-    medians1 = df[labels==0].median(axis=1)
-    std1 = df[labels==0].std(axis=1)
-    maxval1 = df[labels==0].max(axis=1)
-    minval1 = df[labels==0].min(axis=1)
-    skew1 = df[labels==0].skew(axis=1)
-    means2 = df[labels==1].mean(axis=1)
-    medians2 = df[labels==1].median(axis=1)
-    std2 = df[labels==1].std(axis=1)
-    maxval2 = df[labels==1].max(axis=1)
-    minval2 = df[labels==1].min(axis=1)
-    skew2 = df[labels==1].skew(axis=1)
-    fig = plt.figure(figsize=(12,8))
-    ax = fig.add_subplot(231)
-    ax.hist(means1,alpha=0.8,bins=50,color='b',normed=True,range=(-250,250))
-    ax.hist(means2,alpha=0.8,bins=50,color='r',normed=True,range=(-250,250))
-    ax.get_legend()
-    ax.set_xlabel('Mean Intensity')
-    ax.set_ylabel('Num. of Stars')
-    ax = fig.add_subplot(232)
-    ax.hist(medians1,alpha=0.8,bins=50,color='b',normed=True,range=(-0.1,0.1))
-    ax.hist(medians2,alpha=0.8,bins=50,color='r',normed=True,range=(-0.1,0.1))
-    ax.get_legend()
-
-    ax.set_xlabel('Median Intensity')
-    ax.set_ylabel('Num. of Stars')
-    ax = fig.add_subplot(233)    
-    ax.hist(std1,alpha=0.8,bins=50,normed=True,color='b',range=(0,4000))
-    ax.hist(std2,alpha=0.8,bins=50,normed=True,color='r',range=(0,4000))
-    ax.get_legend()
-
-    ax.set_xlabel('Intensity Standard Deviation')
-    ax.set_ylabel('Num. of Stars')
-    ax = fig.add_subplot(234)
-    ax.hist(maxval1,alpha=0.8,bins=50,normed=True,color='b',range=(-10000,50000))
-    ax.hist(maxval2,alpha=0.8,bins=50,normed=True,color='r',range=(-10000,50000))
-    ax.get_legend()
-
-    ax.set_xlabel('Maximum Intensity')
-    ax.set_ylabel('Num. of Stars')
-    ax = fig.add_subplot(235)
-    ax.hist(minval1,alpha=0.8,bins=50,normed=True,color='b',range=(-50000,10000))
-    ax.hist(minval2,alpha=0.8,bins=50,normed=True,color='r',range=(-50000,10000))
-    ax.get_legend()
-
-    ax.set_xlabel('Minimum Intensity')
-    ax.set_ylabel('Num. of Stars')
-    ax = fig.add_subplot(236)
-    ax.hist(skew1,alpha=0.8,bins=50,normed=True,color='b',range=(-40,60))
-    ax.hist(skew2,alpha=0.8,bins=50,normed=True,color='r',range=(-40,60)) 
-    ax.get_legend()
-
-    ax.set_xlabel('Intensity Skewness')
-    ax.set_ylabel('Num. of Stars')
-    stats_plots_label(log_data)
-    plt.show()
 
 #visualise class imbalance
 def vs_class_imbalance(X,y,X_resampled,y_resampled):
@@ -108,8 +52,8 @@ def vs_class_imbalance(X,y,X_resampled,y_resampled):
         ax.get_yaxis().tick_left()
         ax.spines['left'].set_position(('outward', 10))
         ax.spines['bottom'].set_position(('outward', 10))
-        ax.set_xlim([-6, 8])
-        ax.set_ylim([-6, 8])
+        #ax.set_xlim([-6, 8])
+        #ax.set_ylim([-6, 8])
 
     plt.figlegend((c0, c1), ('Class #0', 'Class #1'), loc='lower center',
                   ncol=2, labelspacing=0.)
